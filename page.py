0da73e20f9569bfdf2e1af3dbea5bdf432fcdb25
Reina0326 <badamaarei@gmail.com>
2025-04-10 09:57:40 +0800

    Update page.py

--- page.py
+++ page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from app import show_descriptive_statistics # app.py
 from Hool import show_hool_bolovsruulalt # Hool.py
-
+st.set_page_config(layout="wide")
 # Хуудас сонгох UI
 page = st.sidebar.radio("Хуудас сонгох", ("Descriptive Statistics","Хоол боловсруулалт"))
 
